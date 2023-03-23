9123d61deef5d374e44df5bb7a5832c9d4db9477
Kaloyan Dimitrov <kaloyankd.06@gmail.com>
2023-03-23 11:57:55 +0200

    Added instructions.

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ ESCAPE_KEY = 27
 
 
 class Command:
-    def __init__(self, key, text, msg):
+    def __init__(self, key, text, key_name, msg):
         self.key = key
         self.text = text
+        self.key_name = key_name
         self.msg = msg
 
 
-commands = [Command(curses.KEY_UP, 'Foward...', 'forward'),
-            Command(curses.KEY_DOWN, 'Backward...', 'backward'),
-            Command(curses.KEY_LEFT, 'Left...', 'left'),
-            Command(curses.KEY_RIGHT, 'Right...', 'right'),
-            Command(curses.KEY_PPAGE, 'Robust forward...', 'rforward'),
-            Command(curses.KEY_NPAGE, 'Robust backward...',
+commands = [Command(curses.KEY_UP, 'Foward', 'Arrow Up', 'forward'),
+            Command(curses.KEY_DOWN, 'Backward', 'Arrow Down', 'backward'),
+            Command(curses.KEY_LEFT, 'Left', 'Arrow Left', 'left'),
+            Command(curses.KEY_RIGHT, 'Right', 'Arrow Right', 'right'),
+            Command(curses.KEY_PPAGE, 'Robust forward', 'Page Up', 'rforward'),
+            Command(curses.KEY_NPAGE, 'Robust backward', 'Page Down',
                     'rbackward'),
-            Command(ord(' '), 'Stand...', 'stand'),
-            Command(ord('c'), 'Sit...', 'sit'),
-            Command(ord('x'), 'Lie...', 'lie')] +\
-    [Command(ord(str(i)), f'Emote {i}...', f'emote{i}') for i in range(10)]
+            Command(ord(' '), 'Stand', 'Space', 'stand'),
+            Command(ord('c'), 'Sit', 'c', 'sit'),
+            Command(ord('x'), 'Lie', 'x', 'lie')] +\
+    [Command(ord(str(i)), f'Emote {i}', str(
+        i), f'emote{i}') for i in range(10)]
 
 
 if __name__ == '__main__':
@@ -30,7 +32,7 @@ if __name__ == '__main__':
     port = 5000
 
     client_socket = socket.socket()
-    client_socket.connect((host, port))
+    #  client_socket.connect((host, port))
     os.environ.setdefault('ESCDELAY', '0')
     scr = curses.initscr()
     curses.cbreak()
@@ -40,6 +42,9 @@ if __name__ == '__main__':
     # lower escape delay
 
     scr.addstr(0, 5, "Hit 'Escape' to quit")
+    # add instructions
+    for (ind, command) in enumerate(commands):
+        scr.addstr(4 + ind, 5, f'{command.key_name} -> {command.text}')
     scr.refresh()
     key = ''
     # run until escape
@@ -50,7 +55,7 @@ if __name__ == '__main__':
                 scr.move(2, 0)
                 scr.clrtoeol()
                 scr.addstr(2, 5, cmd.text)
-                client_socket.send(cmd.msg.encode())
+                #  client_socket.send(cmd.msg.encode())
                 break
         scr.refresh()
     curses.endwin()
